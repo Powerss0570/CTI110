41760341e52e572df765fa4d74d92005784d1497
Powerss0570 <55673237+Powerss0570@users.noreply.github.com>
2019-10-06 20:35:35 -0400

    Debugging

---
+++ P3LAB_Debugging_StevenPowers.py
@@ -0,0 +1,20 @@
+#CTI-110-0007
+#P3LAB - Debugging
+#Steven Powers
+#10/6/2019
+
+#Determine the grade.
+
+if score>=A_Score:
+    print('Your grade is A.')
+else:
+    if score>=B_Score:
+        print('Your grade is B.')
+    else:
+        if score>=C_Score:
+            print('Your grade is C.')
+        else:
+            if score>=D_Score
+                print('Your grade is D.')
+            else:
+                print('Your grade is F.')

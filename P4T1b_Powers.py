6857294f84f677c78bfa4376897da50a767220f8
Powerss0570 <55673237+Powerss0570@users.noreply.github.com>
2019-10-13 20:52:47 -0400

    Initials Turtle P4T1b

---
+++ P4T1b_Powers.py
@@ -0,0 +1,77 @@
+# Draw First initials from your first and last name.
+# 10/12/2019
+# CTI-110-0007 P4T1b Initials
+# Steven Powers
+
+import turtle
+
+# Draw my first initial of my name which is "S".
+turtle.pencolor('red')
+turtle.left(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.backward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(20)
+turtle.left(90)
+turtle.backward(10)
+turtle.left(90)
+turtle.forward(10)
+turtle.left(90)
+turtle.forward(40)
+turtle.left(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.backward(50)
+turtle.right(90)
+turtle.backward(60)
+turtle.left(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(20)
+turtle.right(90)
+turtle.forward(10)
+turtle.right(90)
+turtle.forward(10)
+turtle.left(90)
+turtle.forward(40)
+turtle.penup()
+turtle.forward(100)
+
+# Draw the first initial of my last name which is "P".
+turtle.pendown()
+turtle.left(90)
+turtle.forward(90)
+turtle.right(90)
+turtle.forward(60)
+turtle.right(90)
+turtle.forward(50)
+turtle.right(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.forward(50)
+turtle.left(90)
+turtle.backward(10)
+turtle.right(90)
+turtle.backward(90)
+turtle.penup()
+turtle.left(90)
+turtle.forward(10)
+turtle.pendown()
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(30)
+turtle.right(90)
+turtle.forward(40)
+turtle.right(90)
+turtle.forward(30)
+
+
+
+
+
+

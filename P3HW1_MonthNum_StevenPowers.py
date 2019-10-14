3d1873ff21ec03d9931862134de5a583e4e4fe8a
Powerss0570 <55673237+Powerss0570@users.noreply.github.com>
2019-10-13 20:51:43 -0400

    Monthly P3HW1

---
+++ P3HW1_MonthNum_StevenPowers.py
@@ -0,0 +1,35 @@
+#CTI-110-0007
+#P3HW1 - Month number
+#Steven Powers
+#10/6/2019
+
+# Get a month entry from the user.
+Input_month = int(input('Enter your Month entry; '))
+
+# Determine the month.
+if Input_month==1:
+    print('Your month is January.')
+elif Input_month==2:
+    print('Your month is February.')
+elif Input_month==3:
+    print('Your month is March.')
+elif Input_month==4:
+    print('Your month is April.')
+elif Input_month==5:
+    print('Your month is May.')
+elif Input_month==6:
+    print('Your month is June.')
+elif Input_month==7:
+    print('Your month is July.')
+elif Input_month==8:
+    print('Your month is August.')
+elif Input_month==9:
+    print('Your month is September.')
+elif Input_month==10:
+    print('Your month is October.')
+elif Input_month==11:
+    print('Your month is November.')
+elif Input_month==12:
+    print('Your month is December.')
+else: 
+    print('Invalid Entry.')

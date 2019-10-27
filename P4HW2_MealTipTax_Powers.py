5104f25ccaab81e5c6e45e433f99bd28cc1db16f
Powerss0570 <55673237+Powerss0570@users.noreply.github.com>
2019-10-27 19:05:31 -0400

    Add files via upload
    MealTipTax - Run program again

---
+++ P4HW2_MealTipTax_Powers.py
@@ -0,0 +1,41 @@
+# CTI-110-0007
+# P4HW2 - MealTipTax
+# Steven Powers 
+# 10/23/2019
+
+contin = 'y'
+
+while contin == 'y':
+
+    #Ask user to enter the charge for food
+    Meal = float(input("How much did your Meal cost?"))
+
+    #Ask user to enter the tip for Server
+    Tip = float(input("Enter the Tip for server either 16% or 18% or 20%?"))
+    defaulttip = .20
+
+    if Tip == 0.16 or Tip == 0.18 or Tip == 0.20:
+        Tip_Amount = Meal * Tip
+        Tax_Amount = Meal * 0.06
+        Total_cost = Meal + Tax_Amount + Tip_Amount
+
+        print('The calculated tip is $ %.2f' % Tip_Amount)
+
+        print('The calculated tax is $ %.2f' % Tax_Amount)
+
+        print('The calculated Total is $ %.2f' % Total_cost)
+        contin = input("Would you like to run program again and enter another tip y/n?")
+
+    else:
+        Tip_Amount2 = Meal * defaulttip
+        Tax_Amount = Meal * 0.06
+        Total_cost = Meal + Tax_Amount + Tip_Amount2
+        print('Incorrect amount, defaulting to 20%')
+    
+        print('The calculated tip is $ %.2f' % Tip_Amount2)
+
+        print('The calculated tax is $ %.2f' % Tax_Amount)
+
+        print('The calculated Total is $ %.2f' % Total_cost)
+        contin = input("Would you like to run program again and enter another tip y/n?")
+
